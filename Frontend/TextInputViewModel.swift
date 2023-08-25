52fdbcc6bfa484a12d576258b5589485441f0adc
asaini987 <135754860+asaini987@users.noreply.github.com>
2023-08-25 14:42:12 -0400

    Create TextInputViewModel.swift

---
+++ Frontend/TextInputViewModel.swift
@@ -0,0 +1,85 @@
+import SwiftUI
+
+class TextInputViewModel: ObservableObject {
+    
+    struct SummaryRequestBody: Encodable {
+        let text: String
+    }
+    
+    struct SummaryResponseBody: Decodable {
+        let summary: String
+    }
+    
+    struct UrlRequestBody: Encodable {
+        let url: String
+    }
+    
+    struct TextResponseBody: Decodable {
+        let text: String
+    }
+    
+    func fetchText(from link: String) async -> String {
+        //encoding url to JSON
+//        guard let url = URL(string: link) else {
+//            return "Provided link is invalid"
+//        }
+        
+        let requestObject = UrlRequestBody(url: link)
+        guard let encoded = try? JSONEncoder().encode(requestObject) else {
+            return "Cant encode URL to JSON"
+        }
+        
+        //prepping POST request
+        guard let apiLink = URL(string: "http://127.0.0.1:5000/extract_text") else {
+            return "Invalid URL"
+        }
+        
+        var request = URLRequest(url: apiLink)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        //sending request and decoding response
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            guard let response = try? JSONDecoder().decode(TextResponseBody.self, from: data) else {
+                return "Couldn't decode JSON"
+            }
+            
+            return response.text
+            
+        } catch {
+            return "No text could be extracted"
+        }
+    }
+    
+    func fetchSummary(of text: String) async -> String {
+        //encoding text to JSON
+        let requestObject = SummaryRequestBody(text: text)
+        guard let encoded = try? JSONEncoder().encode(requestObject) else {
+            return "Can't encode text to JSON"
+        }
+        
+        //prepping POST request
+        guard let url = URL(string: "http://127.0.0.1:5000/summarize") else {
+            return "Invalid URL"
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        //sending request and decoding response
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            guard let response = try? JSONDecoder().decode(SummaryResponseBody.self, from: data) else {
+                return "Couldn't decode JSON"
+            }
+            
+            return response.summary
+            
+        } catch {
+            print(error.localizedDescription)
+            return "No summary found"
+        }
+    }
+}
